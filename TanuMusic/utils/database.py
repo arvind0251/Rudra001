1c6d0282e6060f57364cadf1555367d002d7edf2
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-18 16:45:24 +0530

    Update database.py

--- TanuMusic/utils/database.py
+++ TanuMusic/utils/database.py
@@ -3,7 +3,9 @@ from typing import Dict, List, Union
 
 from TanuMusic import userbot
 from TanuMusic.core.mongo import mongodb
+from TanuMusic.utils.mongo import db
 
+afkdb = db.afk
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
 autoenddb = mongodb.autoend
@@ -106,6 +108,34 @@ async def get_assistant(chat_id: int) -> str:
             userbot = await set_assistant(chat_id)
             return userbot
 
+async def is_afk(user_id: int) -> bool:
+    user = await afkdb.find_one({"user_id": user_id})
+    if not user:
+        return False, {}
+    return True, user["reason"]
+
+
+async def add_afk(user_id: int, mode):
+    await afkdb.update_one(
+        {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
+    )
+
+
+async def remove_afk(user_id: int):
+    user = await afkdb.find_one({"user_id": user_id})
+    if user:
+        return await afkdb.delete_one({"user_id": user_id})
+
+
+async def get_afk_users() -> list:
+    users = afkdb.find({"user_id": {"$gt": 0}})
+    if not users:
+        return []
+    users_list = []
+    for user in await users.to_list(length=1000000000):
+        users_list.append(user)
+    return users_list
+  
 
 async def set_calls_assistant(chat_id):
     from TanuMusic.core.userbot import assistants
