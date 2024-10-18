a1b9a688a33dc96386a6b46f33916fe890030eeb
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-18 19:03:39 +0530

    Update play.py

--- TanuMusic/utils/inline/play.py
+++ TanuMusic/utils/inline/play.py
@@ -62,7 +62,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="◯", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="◯
+▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
