314b0ac99eec35461df3a070ee18fc7d889f4eb3
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-18 17:08:28 +0530

    Update mongo.py

--- TanuMusic/utils/mongo.py
+++ TanuMusic/utils/mongo.py
@@ -6,15 +6,3 @@ from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.TanuMusic
-
-async def add_wlcm(chat_id: int):
-    return await wlcm.insert_one({"chat_id": chat_id})
-
-# Function to remove a welcome document
-async def rm_wlcm(chat_id: int):   
-    chat = await wlcm.find_one({"chat_id": chat_id})
-    if chat: 
-        return await wlcm.delete_one({"chat_id": chat_id})
-
-
-
