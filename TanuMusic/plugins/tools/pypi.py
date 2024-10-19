32267b70c40a04461c965dbc8a3c1d10810c97e4
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-19 20:21:41 +0530

    Update pypi.py

--- TanuMusic/plugins/tools/pypi.py
+++ TanuMusic/plugins/tools/pypi.py
@@ -1,49 +1,57 @@
 import requests
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
 from TanuMusic import app
 
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/TanuMusicxBot?startgroup=true"),
+    ],
+]
 
 def get_pypi_info(package_name):
     try:
+        
         api_url = f"https://pypi.org/pypi/{package_name}/json"
+        
+        # Sending a request to the PyPI API
         response = requests.get(api_url)
-        if response.status_code == 200:
-            pypi_info = response.json()
-            return pypi_info
-        else:
-            return None
+        
+        # Extracting information from the API response
+        pypi_info = response.json()
+        
+        return pypi_info
+    
     except Exception as e:
-        print(f"Error fetching PyPI information: {e}")
+        print(f"⬤ Error fetching PyPI information ➥ {e}")
         return None
 
-
 @app.on_message(filters.command("pypi", prefixes="/"))
-async def pypi_info_command(client, message):
+def pypi_info_command(client, message):
     try:
+       
         package_name = message.command[1]
+        
+        # Getting information from PyPI
         pypi_info = get_pypi_info(package_name)
-
+        
         if pypi_info:
-            info_message = (
-                f"ᴅᴇᴀʀ {message.from_user.mention} \n "
-                f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴘᴀᴋᴀɢᴇ ᴅᴇᴛᴀɪʟs \n\n "
-                f"ᴘᴀᴋᴀɢᴇ ɴᴀᴍᴇ ➪ {pypi_info['info']['name']}\n\n"
-                f"ʟᴀᴛᴇsᴛ ᴠᴇʀsɪᴏɴ ➪ {pypi_info['info']['version']}\n\n"
-                f"ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ➪ {pypi_info['info']['summary']}\n\n"
-                f"ᴘʀᴏJᴇᴄᴛ ᴜʀʟ ➪ {pypi_info['info']['project_urls']['Homepage']}"
-            )
-            close_markup = InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close")]]
-            )
-            await message.reply_text(info_message, reply_markup=close_markup)
+            # Creating a message with PyPI information
+            info_message = f"❖ ᴘᴀᴄᴋᴀɢᴇ ɴᴀᴍᴇ ➥ `{pypi_info['info']['name']}`\n\n" \
+                           f"● ʟᴀᴛᴇsᴛ ᴠᴇʀsɪᴏɴ ➥ `{pypi_info['info']['version']}`\n\n" \
+                           f"● ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ➥ {pypi_info['info']['summary']}\n\n" \
+                           f"❖ ᴘʀᴏᴊᴇᴄᴛ ᴜʀʟ ➥ {pypi_info['info']['project_urls']['Homepage']}"
+            
+            
+            client.send_message(message.chat.id, info_message, reply_markup=InlineKeyboardMarkup(EVAA),
+    )
+        
         else:
-            await message.reply_text(
-                f"Package '{package_name}' not found \n please dont try again later ."
-            )
-
+            # Handling the case where information retrieval failed
+            client.send_message(message.chat.id, "⬤ Failed to fetch information from PyPI.")
+    
     except IndexError:
-        await message.reply_text(
-            "Please provide a package name after the /pypi command."
-        )
+
+        client.send_message(message.chat.id, "⬤ Please provide a package name after the /pypi command.")
+
+
